2d67a4494aa82b40946cce5d62a9f2ee4df531d4
Julio Donado <julio22567@hotmail.com>
2023-08-25 15:49:09 -0500

    Definimos el enrutador principal

---
+++ README.md
@@ -0,0 +1,20 @@
+<!-- my-100-movie-list -->
+# Proyecto API My 100 Movie List
+
+## Descripción
+Este proyecto consiste en desarrollar una REST API de una red social en la cual los amantes del cine compartan los títulos de películas que más les gustan, el propósito es poder compartir y calificar sus preferencias con el fin de
+enriquecer mutuamente su lista de películas.
+
+## Tecnologías
+- Node.js
+- Express.js
+- MongoDB
+- JWT para autenticación
+- Mongoose para modelado y acceso a la base de datos
+- Jest para pruebas unitarias
+
+## Instalación y Ejecución
+1. Clona este repositorio.
+2. Instala las dependencias usando `npm install`.
+3. Configura las variables de entorno, como la conexión a la base de datos y la clave secreta JWT.
+4. Ejecuta `npm start` para iniciar el servidor.
--- www/server.js
+++ www/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors'
+import router from "../routers/router.js";
 
 class Server {
 
@@ -9,10 +10,32 @@ class Server {
         this.port = process.env.PORT || 3000
 
         // ! Definimos los string rutas
-        this.authPath = '/api/auth'
+        this.authPath = '/api'
 
+        // ! Middlewares
+        this.midleware()
 
+        // ! Rutas de mi aplicacion
+        this.routes()
     }
+
+    midleware() {
+        // ! CORS
+        this.app.use(cors())
+
+        // ! Lectura y parseo del body
+        this.app.use(express.json())
+
+        // ! Directorio publico
+        this.app.use(express.static('public'))
+    }
+
+    routes() {
+        // ! Llamamos la ruta principal /api donde heredaran las demas
+        this.app.use(this.authPath, router)
+        // this.app.use(this.usuariosPath, require('../routes/usuarios.routes'))
+    }
+
 
     listen(){
         this.app.listen(this.port, () => {
